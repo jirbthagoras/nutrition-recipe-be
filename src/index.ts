f22343ef29e6dc6bf35503973e8717ee5fd8c353
jirbthagoras <hansjabriel@gmail.com>
2025-04-08 08:14:13 +0700

    [+] Allow CORS

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,10 @@ import { cookie } from "express-validator";
 dotenv.config();
 
 const app: Express = express();
+const cors = require("cors")
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
 app.use("/api/v1/", v1Route);
